e6404edd765f11ec1dfb0f1e0a3c801deda0012c
math <matheusmourinha16@gmail.com>
2023-06-13 21:28:06 -0300

    continuação no projeto

---
+++ JS/main.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("novoItem")
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
+
+function criaElemento(nome, quantidade) {
+    console.log(nome)
+    console.log(quantidade)
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+    
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild = numeroItem + nome
+}
