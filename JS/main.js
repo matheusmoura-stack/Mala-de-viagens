ca80912b1ea8c470beab1cbfcc8234cade669284
math <matheusmourinha16@gmail.com>
2023-06-20 19:08:05 -0300

    Projeto finalizado

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,6 @@ const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-
 itens.forEach( (elemento) => {
     criaElemento(elemento)
 } )
@@ -15,25 +14,22 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
-
-//  Const para conferir elemento nome no array itens 
-    const existe = itens.find( elemento => elemento.nome === nome.value ) 
+    const existe = itens.find( elemento => elemento.nome === nome.value )
 
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-
-// Condicional para conferir se o elemento 
     if (existe) {
         itemAtual.id = existe.id
 
         atualizaElemento(itemAtual)
-
-        itens[existe.id] = itemAtual
+        
+//Refatoração da condicional if else, atualizando um id para cada item
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-       itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
 
         criaElemento(itemAtual)
 
@@ -46,7 +42,6 @@ form.addEventListener("submit", (evento) => {
     quantidade.value = ""
 })
 
-
 function criaElemento(item) {
     const novoItem = document.createElement("li")
     novoItem.classList.add("item")
@@ -58,7 +53,7 @@ function criaElemento(item) {
 
     novoItem.innerHTML += item.nome
 
-    novoItem.appendChild(botaoDeleta())
+    novoItem.appendChild(botaoDeleta(item.id)) // Referenciar a função botaoDeleta no nó da função principal
 
     lista.appendChild(novoItem)
 }
@@ -67,17 +62,24 @@ function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
 
-function botaoDeleta() {
+//Função para criar botão com evento de click nos itens, e retornar os itens clicados
+function botaoDeleta(id) {
     const elementoBotao = document.createElement("button")
     elementoBotao.innerText = "X"
 
     elementoBotao.addEventListener("click", function() {
-        deletaElemento(this.parentNode)
+        deletaElemento(this.parentNode, id)
     })
 
     return elementoBotao
 }
 
-function deletaElemento(tag) {
+//Função para deletar os itens enviados da função botaoDeleta no array de itens e no navegador
+
+function deletaElemento(tag, id) { 
     tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
